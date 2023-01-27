c1fae450f038103ef8ccec5b8a723fdf80319c78
Werton <mr.werton@gmail.com>
2023-01-27 12:42:01 -0300

    feat: added the value in id when it is null

--- src/core/entity/index.ts
+++ src/core/entity/index.ts
@@ -1,9 +1,11 @@
-abstract class Entity<T>{
+import {randomUUID} from 'node:crypto'
+
+export abstract class Entity<T>{
     public data: T
     public id?: string
 
     constructor(data: T, id?:string){
-        this.id = id;
+        this.id = id ?? randomUUID();
         this.data = data;
     }
 

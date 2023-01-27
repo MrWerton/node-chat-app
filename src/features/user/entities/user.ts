01b8f9e34c005e017e3ca4d5c61d22f8f5a81d97
Werton <mr.werton@gmail.com>
2023-01-27 12:43:04 -0300

    feat: created user entity

--- src/features/user/entities/user.ts
+++ src/features/user/entities/user.ts
@@ -1,15 +1,22 @@
-import {randomUUID} from 'node:crypto'
+import { Entity } from "../../../core/entity";
 
-export class User{
-    id: string;
+interface UserProps{
     name: string;
     email: string;
     password: string;
     photo_url: string;
 
-    constructor(){
-        if(!this.id){
-            this.id = randomUUID()
-        }
+}
+
+export class User extends Entity<UserProps>{
+   
+    private constructor(data: UserProps, id?: string){
+      super(data, id)
+    }
+
+    static create(data: UserProps, id?: string){
+        const user = new User(data, id);
+
+        return user;
     }
 }
